e6b7a6800643e46ae924150b670d92aa7c51ec37
Michael Housh <mhoush@houshhomeenergy.com>
2022-01-17 10:36:36 -0500

    Bumped version

--- Formula/swift-zet.rb
+++ Formula/swift-zet.rb
@@ -2,7 +2,7 @@ class SwiftZet < Formula
   desc "A command line tool for managing Zettelkasten repositories."
   homepage "https://github.com/m-housh/swift-zet"
   url "https://github.com/m-housh/swift-zet.git",
-      tag: "0.1.0", revision: "877871788bd196299ab80aa2463ab615f0b18cc9"
+      tag: "0.1.1", revision: "bdc55fe59317a59177677d28ba36d5de8c9e0c70"
   head "https://github.com/m-housh/swift-zet.git"
 
   depends_on xcode: ["10.15", :build]
