3c266d619dd916e5183d0cb13521e85bf37ff050
Michael Housh <m-housh@users.noreply.github.com>
2022-01-25 15:00:30 -0500

    Update swift-zet to 0.3.6

--- Formula/swift-zet.rb
+++ Formula/swift-zet.rb
@@ -5,11 +5,7 @@ class SwiftZet < Formula
 
   license: "Apache-2.0"
 
-  bottle do
-    root_url "https://github.com/m-housh/swift-zet/releases/download/0.3.0"
-    rebuild 1
-    sha256 cellar: any, big_sur: ""
-  end
+  
 
   head "https://github.com/m-housh/swift-zet.git", shallow: false
 
