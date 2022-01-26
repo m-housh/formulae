d0aafcb6e36760ce49bf5bcca9ff85e25aaa04ef
Michael Housh <m-housh@users.noreply.github.com>
2022-01-26 08:59:59 -0500

    Update swift-zet to 0.3.10

--- Formula/swift-zet.rb
+++ Formula/swift-zet.rb
@@ -1,16 +1,11 @@
 class SwiftZet < Formula
   desc "A command line tool for managing Zettelkasten repositories."
   homepage "https://github.com/m-housh/swift-zet"
-  url "https://github.com/m-housh/swift-zet.git", tag: "0.3.7", revision: "50cf70e7273c1c4ff5188d2a90b4191a24408678"
+  url "https://github.com/m-housh/swift-zet.git", tag: "0.3.10", revision: "dc5386d0ddf7d852c4653e3ebeb5656af2715164"
 
   license "Apache-2.0"
 
-    bottle do
-      root_url "https://github.com/m-housh/swift-zet/releases/download/0.3.7"
-      rebuild 2
-      sha256 cellar: :any, big_sur: "ccb2ee344fb49307a610a2102fc98f9ca1f9ae3df0b156de35dadb760bd3366c"
-      sha256 cellar: :any_skip_relocation, arm64_monterey: "3b15d02ced305aa84d54732b891ea330a64160530936e4a43fa0aee18c3284e4"
-    end
+    
 
 
 
