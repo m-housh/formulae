5cf69be8eb694bcca8b3768222204b76bdff38fd
Michael Housh <mhoush@houshhomeenergy.com>
2023-03-05 19:21:02 -0500

    wip

--- Formula/dots.rb
+++ Formula/dots.rb
@@ -15,6 +15,10 @@ class Dots < Formula
     (zsh_completion/"dots").write `#{bin}/dots --generate-completion-script zsh`
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "0af03cf512e2e49ffa74bb6ac69ebd8c622504255d5f33cf7ee4da93448dce6e"
+  end
+
   test do
     system "#{bin}/dots" "--version"
   end
