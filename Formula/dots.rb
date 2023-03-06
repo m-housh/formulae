2068c56e036c5e6cae2c1c1469eaef0c38fcdeb3
Michael Housh <mhoush@houshhomeenergy.com>
2023-03-06 14:17:19 -0500

    Updated dots to version 0.0.4

--- Formula/dots.rb
+++ Formula/dots.rb
@@ -2,13 +2,12 @@ class Dots < Formula
   desc "Command-line tool for managing my dotfiles"
   homepage "https://github.com/m-housh/dots"
   url "https://github.com/m-housh/dots.git", branch: "main"
-  version "0.0.3"
+  version "0.0.4"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/m-housh/dots/releases/download/0.0.3"
-    rebuild 5
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "17085de78ce4c6182272633eb8869ee5900f758d1bfc3012f2a8963d364778bf"
+    root_url "https://github.com/m-housh/dots/releases/download/0.0.4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "b0d59ca6a0f6933e79636912776ebd50bce4208f850e8e83fbfcc31394da415a"
   end
 
   depends_on xcode: ["14.2", :build]
