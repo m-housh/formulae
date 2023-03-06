2a612383e82fa82f813cf3df83418666915d8ffb
Michael Housh <mhoush@houshhomeenergy.com>
2023-03-06 11:29:55 -0500

    Dots v0.0.3

--- Formula/dots.rb
+++ Formula/dots.rb
@@ -1,22 +1,21 @@
 class Dots < Formula
   desc "Command-line tool for managing my dotfiles"
   homepage "https://github.com/m-housh/dots"
-  url "https://github.com/m-housh/dots/releases/download/0.0.3/dots--0.0.3.arm64_ventura.bottle.2.tar.gz"
+  url "https://github.com/m-housh/dots.git", branch: "main"
   version "0.0.3"
   license "MIT"
+
+  bottle do
+    root_url "https://github.com/m-housh/dots/releases/download/0.0.3"
+    rebuild 5
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "17085de78ce4c6182272633eb8869ee5900f758d1bfc3012f2a8963d364778bf"
+  end
 
   depends_on xcode: ["14.2", :build]
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
-    (bash_completion/"dots").write `#{bin}/dots --generate-completion-script bash`
-    (fish_completion/"dots.fish").write `#{bin}/dots --generate-completion-script fish`
-    (zsh_completion/"dots").write `#{bin}/dots --generate-completion-script zsh`
-  end
-
-  bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "60219fa9a287063456d42f1f761e0d1d760fdeb7246ea45f65511eae0a93cdde"
+    generate_completions_from_executable(bin/"dots", "--generate-completion-script")
   end
 
   test do
